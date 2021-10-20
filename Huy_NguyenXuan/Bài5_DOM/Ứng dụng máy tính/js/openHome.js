f616e6a6fabd9281e6d95c03b1949cad86e00cec
Juanvu <hoanvd1210@gmail.com>
2021-10-20 11:46:32 +0700

    -Sua bai Huy: đặt tên biến mang ý nghĩa hơn. Sử dụng hàm để truyền tham số, đối với các nút nhập số. Nên đặt file js load dưới cùng thẻ đóng </body>. ko thì nên có sự kiện onload để đảm bảo HTML được tải xong. tránh gây lỗi ở file js

--- Huy_NguyenXuan/Bài5_DOM/Ứng dụng máy tính/js/openHome.js
+++ Huy_NguyenXuan/Bài5_DOM/Ứng dụng máy tính/js/openHome.js
@@ -10,3 +10,18 @@ function offBrack() {
     let innerTextBox = document.getElementById('innerTextBox').value;
     innerTextBox.innerHTML = innerTextBox + offBrack;
 }
+
+function numberClick(val) {
+	//co the kiem tra xem dung la so khong
+	document.getElementById('innerTextBox').value += val;
+}
+function showResult(){
+	const result = eval(document.getElementById('innerTextBox').value);
+	
+	document.getElementById('innerTextBox').value += ("=" + result);
+}
+
+function operatorClikc(opr){
+	//kiem tra xem dung la nhung toan tu hop le ko
+	document.getElementById('innerTextBox').value += opr;
+}

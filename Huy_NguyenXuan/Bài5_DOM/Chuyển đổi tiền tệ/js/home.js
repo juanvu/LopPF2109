34a0dcb968b0adbb16c60516616f9ee88d632a5b
Nguyen Xuan Huy <huynx42@due.edu.vn>
2021-10-21 00:38:30 +0700

    update file bai5_DOM

--- Huy_NguyenXuan/Bài5_DOM/Chuyển đổi tiền tệ/js/home.js
+++ Huy_NguyenXuan/Bài5_DOM/Chuyển đổi tiền tệ/js/home.js
@@ -2,20 +2,20 @@ function moneyConvert() {
     let moneyConvert = Number(document.getElementById('moneyConvert').value);
     let moneyFrom = document.getElementById('moneyFrom').value;
     let moneyTo = document.getElementById('moneyTo').value;
-    if (moneyFrom = 'USD') {
-        if (moneyTo = 'VND') {
+    if (moneyFrom == 'USD') {
+        if (moneyTo == 'VND') {
             var moneyRate = 22760.22;
             var result = moneyConvert * moneyRate;
-        } else if (moneyTo = 'Yên') {
+        } else if (moneyTo == 'Yên') {
             var moneyRate = 114.23;
             var result = moneyConvert * moneyRate;
         } else {
             var result = moneyConvert;
         }
-    } else if (moneyFrom = 'VND') {
-        if (moneyTo = 'VND') {
+    } else if (moneyFrom == 'VND') {
+        if (moneyTo == 'VND') {
             var result = moneyConvert;
-        } else if (moneyTo = 'USD') {
+        } else if (moneyTo == 'USD') {
             var moneyRate = 0.000044;
             var result = moneyConvert * moneyRate;
         } else {
@@ -23,18 +23,15 @@ function moneyConvert() {
             var result = moneyConvert * moneyRate;
         }
     } else {
-        if (moneyTo = 'VND') {
+        if (moneyTo == 'VND') {
             var moneyRate = 199.08;
             var result = moneyConvert * moneyRate;
-        } else if (moneyTo = 'USD') {
+        } else if (moneyTo == 'USD') {
             var moneyRate = 0.0087;
             var result = moneyConvert * moneyRate;
         } else {
             var result = moneyConvert;
         }
     }
-}
-function displayResult() {
-    moneyConvert();
     document.getElementById('displayResult').innerHTML = '<p id="displayResult">' + result + '</p>';
 }

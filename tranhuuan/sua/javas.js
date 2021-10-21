6877d02f6f7f40bec3072c2efd0b9f4cc6685f4f
Vu Duc Hoan <hoan.vd@nsrp.com.vn>
2021-10-21 10:39:13 +0700

    update: bài sửa của Ân

--- tranhuuan/sua/javas.js
+++ tranhuuan/sua/javas.js
@@ -1,29 +1,55 @@
 
-function myfunction(){
-    
-    let s1=document.getElementById("s1").value;
-    let s2=document.getElementById("s2").value;
-    function addtion(s1,s2){
+    function addtion(){
+		let s1=document.getElementById("s1").value;
+		let s2=document.getElementById("s2").value;
         let cong;
         cong=parseInt(s1)+parseInt(s2);
         document.getElementById("output").innerHTML='<strong>'+cong+'</strong>';
     }
     
-    function subtraction(s1,s2){
+    function subtraction(){
+		let s1=document.getElementById("s1").value;
+		let s2=document.getElementById("s2").value;
         let tru;
         tru=parseInt(s1)-parseInt(s2);
         document.getElementById("output").innerHTML='<strong>'+tru+'</strong>';
     }
     
-    function multiplication(s1,s2){
+    function multiplication(){
+		let s1=document.getElementById("s1").value;
+		let s2=document.getElementById("s2").value;
         let nhan;
-        nhan=parseInt(s1)*parseInt(s2);
+        nhan=parseInt(s1)*parseInt();
         document.getElementById("output").innerHTML='<strong>'+nhan+'</strong>';
     }
     
-    function division(s1,s2){
+    function division(){
+		let s1=document.getElementById("s1").value;
+		let s2=document.getElementById("s2").value;
         let chia;
-        chia=parseInt(s1)/parseInt(s2);
+        chia=parseInt(s1)/parseInt();
         document.getElementById("output").innerHTML='<strong>'+chia+'</strong>';
     }
-}
+	
+	/*
+	cách viết 2 sử dụng hàm onload, cách này thì em phải sử dụng cách viết bind sự kiên onclick vào thẻ.
+	
+	gia su the button cua em dang khai bao nhu nay
+	    <input type="button" value="Addtion(+)" onclick="addtion()" id='btnAdd'/> 
+		
+	//đoạn nay bind sự kiên click cho thẻ button có id là btnAdd
+	
+	
+	
+	
+	lúc đó cái myfunction của em troong se sẽ như này
+	
+	function myfunction() {
+		
+		 document.getElementById('btnAdd').addEventListener('click', function () {
+		 		let s1=document.getElementById("s1").value;
+		      let s2=document.getElementById("s2").value;	
+        result.innerHTML += this.value;
+    });
+	}
+	*/

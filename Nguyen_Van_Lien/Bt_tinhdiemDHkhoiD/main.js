578326b2faea9aa01e967e75d5d2349699e6c6b9
DADENSPPC0289\daden <daden92bg@gmail.com>
2021-10-23 20:57:29 +0900

    add index.html main.js file

---
+++ Nguyen_Van_Lien/Bt_tinhdiemDHkhoiD/main.js
@@ -0,0 +1,27 @@
+let showResult = function() {
+    let firstInput = document.getElementById('inputMath').value;
+    let secondInput = document.getElementById('inputLiterature').value;
+    let thirdInput = document.getElementById('inputEnglish').value;
+    let fourthInput = document.getElementById('selectDistrict').value;
+
+    let sum = 0;
+    
+    switch(fourthInput) {
+        case 'KV1':
+            sum += 0.75;
+            break;
+        case 'KV2':
+            sum += 0.5;
+            break;
+        case 'KV2-Nt':
+            sum += 0.25;
+            break;
+        case 'KV3':
+            sum = result;
+            break;
+    }
+
+    let result = parseInt(firstInput) + parseInt(secondInput) + parseInt(thirdInput) + sum;
+
+    document.getElementById('result').innerText = 'Tổng điểm ĐH khối D của bạn là: ' + result;
+}

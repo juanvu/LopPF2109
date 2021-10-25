38c673ba5a7cacabd0b7fe843d836f1a8d3abcf6
Nguyen Xuan Huy <huynx42@due.edu.vn>
2021-10-25 18:23:50 +0700

    add file Bai6

---
+++ Huy_NguyenXuan/Bài6_If-else-Switch-case/TH_luyentapSwitch-Case/js/home3.js
@@ -0,0 +1,19 @@
+let value = Number(prompt('Type a number', 0));
+// if (value > 0) {
+//     alert('1');
+// }   else if (value < 0) {
+//     alert('-1');
+// }   else {
+//     alert('0');
+// }
+
+switch (value) {
+    case :
+        alert('1');
+        break;
+    case :
+        alert('-1');
+        break;
+    default:
+        alert('0');
+}
---
+++ Huy_NguyenXuan/Bài6_If-else-Switch-case/TH_luyentapifelse/js/home.js
@@ -0,0 +1,6 @@
+let answer = prompt("“What is the “official” name of JavaScript?”");
+if (answer == "ECMAScript") {
+    alert("Right!");
+}   else {
+    alert("Didn’t know? ECMAScript!");
+}

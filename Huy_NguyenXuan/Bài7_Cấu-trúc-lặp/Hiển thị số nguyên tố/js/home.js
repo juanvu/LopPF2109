f023fa35a51fe1b6f7edfec8607f0e11494af418
Nguyen Xuan Huy <huynx42@due.edu.vn>
2021-10-30 21:13:21 +0700

    fix file bai7

--- Huy_NguyenXuan/Bài7_Cấu-trúc-lặp/Hiển thị số nguyên tố/js/home.js
+++ Huy_NguyenXuan/Bài7_Cấu-trúc-lặp/Hiển thị số nguyên tố/js/home.js
@@ -1,11 +1,29 @@
-let number = Number(prompt("Mời nhập số lượng số nguyên tố cần in ra"));
-let count=0;
-for ( count=0; count<number; count++) {
-    for (var n=2; n<1000; n++) {
-        for (var i=2; i < n;i++) {
-            if (n % i !== 0){
-                document.write(n);
+let n = Number(prompt("Mời nhập số lượng số nguyên tố cần in ra"));
+function in_snt(n) {
+    let count;
+    for 
+}
+function kiem_tra_snt(cout) {
+    // Biến cờ hiệu
+    let flag = true;
+ 
+    // Nếu n bé hơn 2 tức là không phải số nguyên tố
+    if (n < 2){
+        flag = false;
+    }
+    else{
+        // lặp từ 2 tới n-1
+        for (var i = 2; i < n-1; i++)
+        {
+            if (n % i == 0){
+                flag = false;
+                break;
             }
         }
+    }
+ 
+    // Kiểm tra biến flag
+    if (flag == true){
+        document.write(n);
     }
 }

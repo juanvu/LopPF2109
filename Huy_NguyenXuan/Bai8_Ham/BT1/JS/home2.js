7e42867ffcd56be7b44d9cd74895ab1e50f70581
Nguyen Xuan Huy <huynx42@due.edu.vn>
2021-11-08 16:07:08 +0700

    update bai8 tu dien

---
+++ Huy_NguyenXuan/Bai8_Ham/BT1/JS/home2.js
@@ -0,0 +1,22 @@
+let dics = [
+    {"key": "hello","vietnam": "xin chao"},
+    {"key": "goodbye","vietnam": "tam biet"},
+    {"key": "sun","vietnam": "mat troi"},
+];
+let userInput = prompt("Moi nhap tu can dich");
+if (userInput) {
+    foundResult = false;
+    for (let i=0;i <dics.length; i++) {
+        if(dics[i].key.toUpperCase() == userInput.toUpperCase()) {
+            let foundDic = dics[i];
+            foundResult = true;
+        alert("Dich ra vietnamese la: " + foundDic.vietnam);
+        break;
+        } 
+    }
+    if (!foundResult) {
+        alert("Không tim thay trong tu dien");
+    }
+} else {
+    alert("De nghi nhap du lieu");
+}

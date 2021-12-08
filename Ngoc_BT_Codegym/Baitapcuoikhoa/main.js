cefe12f7c20560fc7b9b0c216cb61ed723fafe9d
Ngocpt <ngocpt1308@gmail.com>
2021-12-09 07:04:58 +0900

    Ngoc add file

--- Ngoc_BT_Codegym/Baitapcuoikhoa/main.js
+++ Ngoc_BT_Codegym/Baitapcuoikhoa/main.js
@@ -1,3 +1,5 @@
+let count_update = 0;
+
 let items = [
 	{
 		LuaChon: "<input type='checkbox'/>",
@@ -25,7 +27,7 @@ let genItemTable = function () {
 		for (var i = 0; i < items.length; i++) {
 			let item = items[i]; // lấy ra item cụ thể trong mảng items
 			let rowPart = `<tr>
-				<td>${item.LuaChon}</td>
+				<td> <input type="checkbox"></td>
 				<td>${item.MaHang}</td>
 				<td>${item.TenHang}</td>
 				<td>${item.SoLuong}</td>
@@ -42,7 +44,7 @@ let genItemTable = function () {
 			rowParts += rowPart;
 		}
 
-		myNode.innerHTML = rowItems;
+		myNode.innerHTML = rowParts;
 	}
 };
 
@@ -51,15 +53,16 @@ genItemTable();
 
 let LuuHang = function () {
 	//đoạn này lấy giá trị trong form thêm mới hàng
-	let luaChon_1 = document.getElementById("txtLuaChon").innerHTML;
-	luaChon_1 = '<input type="checkbox" id="txtLuaChon" name="selectMore"/>' ;
-	let luaChon = luaChon_1;
+	//let luaChon_1 = document.getElementById("txtLuaChon").innerHTML;
+	//luaChon_1 = '<input type="checkbox" id="txtLuaChon" name="selectMore"/>' ;
+	//let luaChon = luaChon_1;
 
 	let maHang = document.getElementById("txtMaHang").value;
-	let tenHang = document.getElementById("txtTenhang").value;
+	let tenHang = document.getElementById("txtTenHang").value;
 	let soLuong = document.getElementById("txtSoLuong").value;
 	let giaMua = document.getElementById("txtGiaMua").value;
 	let ngayNhap = document.getElementById("txtNgayNhap").value;
+
 	let nhanHang = document.getElementById("txtNhanHang").value;
 
 	//kiểm tra dữ liệu hợp lệ
@@ -73,8 +76,8 @@ let LuuHang = function () {
 	);
 	if (isHangHopLe) {
 		let item = {
-			LuaChon: luaChon,
-			Mahang: maHang,
+			//LuaChon: luaChon,
+			MaHang: maHang,
 			TenHang: tenHang,
 			SoLuong: soLuong,
 			GiaMua: giaMua,
@@ -134,11 +137,82 @@ let xoaHang = function (maHang) {
 			let item = items[i]; // lấy ra item cụ thể trong mảng items
 
 			//nếu mã sinh viên truyền vào, trùng với mã sinh viên trong mảng items, thì sẽ xóa sinh viên đó
-			if (item.MaSV.toUpperCase() == maHang.toUpperCase()) {
+			if (item.MaHang.toUpperCase() == maHang.toUpperCase()) {
 				items.splice(i, 1);
 			}
 		}
 		genItemTable();
 	}
 }
+ function suaHang (maHang) {
 
+	//hàm confirm: bật lên thông báo xác nhận
+	if(confirm(`Bạn có sửa hàng ${maHang} không?`)) {
+		for (var i = 0; i < items.length; i++) {
+			let item = items[i]; // lấy ra item cụ thể trong mảng items
+
+			//nếu mã hàng truyền vào, trùng với mã hàng trong mảng items, thì sẽ xóa hàng đó
+			if(item.MaHang.toUpperCase() == maHang.toUpperCase()) {
+				document.getElementById("txtMaHang").value = item.MaHang;
+				document.getElementById("txtTenHang").value = item.TenHang;
+				document.getElementById("txtSoLuong").value = item.SoLuong;
+				document.getElementById("txtGiaMua").value = item.GiaMua;
+				document.getElementById("txtNgayNhap").value = item.NgayNhap;
+				document.getElementById("txtNhanHang").value = item.NhanHang;
+				
+				// Ban đầu nút Lưu hiện còn nút Update ta ẩn đi
+				// Sau khi nhấn nút Sửa ta sẽ cho nút Lưu ẩn còn nút Update hiện ra
+				document.getElementById("btnUpdate").type = "button";
+				document.getElementById("btnSave").type = "hidden";
+
+			}
+		}
+			genItemTable();
+	}
+}
+function UpdateHang() {
+	let maHang = document.getElementById("txtMaHang").value;
+	let tenHang = document.getElementById("txtTenHang").value;
+	let soLuong = document.getElementById("txtSoLuong").value;
+	let giaMua = document.getElementById("txtGiaMua").value;
+	let ngayNhap = document.getElementById("txtNgayNhap").value;
+	let nhanHang = document.getElementById("txtNhanHang").value;
+
+	//kiểm tra dữ liệu hợp lệ
+	let isHangHopLe = kiemTraDuLieuHang(
+		maHang,
+		tenHang,
+		soLuong,
+		giaMua,
+		ngayNhap,
+		nhanHang,
+	);
+	if (isHangHopLe) {
+		let item = {
+			//LuaChon: luaChon,
+			MaHang: maHang,
+			TenHang: tenHang,
+			SoLuong: soLuong,
+			GiaMua: giaMua,
+			NgayNhap: ngayNhap,
+			NhanHang: nhanHang,
+		};
+
+		for (var i = 0; i < items.length; i++) {
+			if(count_update == i) {
+				items.splice(i, 1, item);
+				break;
+			}
+		}
+
+		genItemTable();
+
+		//sau khi lưu dữ liệu xong, clear form
+		document.getElementById("newItemForm").reset();
+
+		// Nút Update giờ đang hiện vì ta bắt sự kiện nút sửa còn nút Lưu bị ẩn đi
+		// Sau khi nhấn nút Update ta sẽ cho nút Lưu hiện ra còn nút Update lại ẩn đi
+		document.getElementById("btnSave").type = "button";
+		document.getElementById("btnUpdate").type = "hidden";
+	}
+}
